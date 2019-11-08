0336a117fa7287d51f3f722828fd2fc536c93b4f
Arman Schwarz <armanschwarz@gmail.com>
2019-11-09 00:42:29 +1100

    refactor

--- gnc_attachment_check.py
+++ gnc_attachment_check.py
@@ -18,24 +18,26 @@ def main():
 
     slot_values = doc.getElementsByTagName('slot')
 
-    file_count = 0
     error_count = 0
+    file_paths = []
+
     for x in slot_values:
         slot_key = x.getElementsByTagName('slot:key')[0].firstChild.data
 
         if slot_key == 'assoc_uri':
-            file_count += 1
-
             rel_path = request.url2pathname(
                 urllib.parse.urlparse(
                     x.getElementsByTagName('slot:value')[0].firstChild.data).path)
-            full_path = os.path.join(args.base_path, rel_path)
 
-            if not os.path.exists(full_path):
-                error_count += 1
-                print("Failed to find {}...".format(full_path))
+            file_paths.append(os.path.join(args.base_path, rel_path))
 
-    print("found {} errors in {} files!".format(error_count, file_count))
+    print("found {} files to search...".format(len(file_paths)))
+    errors = [x for x in file_paths if not os.path.exists(x)]
+
+    for e in errors:
+        print("Failed to find {}...".format(e))
+
+    print("found {} errors in {} files!".format(error_count, len(file_paths)))
 
 if __name__ == "__main__":
     main()
