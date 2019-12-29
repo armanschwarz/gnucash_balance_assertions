d4df9d253d779b24611d5285190d3069de6592fc
Arman Schwarz <armanschwarz@gmail.com>
2019-12-28 18:38:44 -0800

    start dates for assertions (untested)

--- README.md
+++ README.md
@@ -7,7 +7,15 @@ For help:
 ./gnc_balance_assertion.py --help
 ```
 
-To run assertions to `2` decimal places with the GnuCash file located at `/path/to/account.gnucash`, and where assertions are written as transaction descriptions of the form `Balance Assertion: 123.45`:
+To run assertions for a GnuCash file located at `/path/to/account.gnucash`, and where assertions are written as transaction descriptions of the form `Balance Assertion: 123.45`, you would run the following:
 ```
-./gnc_balance_assertion.py path/to/account.gnucash "(?<=Balance Assertion: )[\-]*\d*\.\d\d" 2
+./gnc_balance_assertion.py path/to/account.gnucash --assertion_amount_regex "(?<=Balance Assertion: )[\-]*\d*\.\d\d"
 ```
+
+If you also want to reconcile accumulated amounts since a specific date, you might want balance assertions that look like this: Balance Assertion: 123.45 (since 2019-07-01):
+
+```
+./gnc_balance_assertion.py path/to/account.gnucash --asertion_amount_regex "(?<=Balance Assertion: )[\-]*\d*\.\d\d" --assertion_start_regex "(?<=\(since: ) \d\d\d\d-\d\d-\d\d(?<\))"
+```
+
+Note that any date format can be used as long as it is natively recognised by [pandas.to_datetime()](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.to_datetime.html)
--- gnc_balance_assertion.py
+++ gnc_balance_assertion.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
+from xml.dom import minidom
 import argparse
 import datetime
+import pandas
 import regex
-from xml.dom import minidom
 
 import util
 
 def main():
     parser = argparse.ArgumentParser(description='Balance assertions for GnuCash')
     parser.add_argument('gnucash_file')
-    parser.add_argument('assertion_regex')
-    parser.add_argument('-d', type=int, default=2, help='number of decimal places for comparison')
+    parser.add_argument('--assertion_amount_regex')
+    parser.add_argument('--assertion_start_regex', default=None)
+    # parser.add_argument('-d', type=int, default=2, help='number of decimal places for comparison')
     args = parser.parse_args()
 
     doc = minidom.parseString(util.read(args.gnucash_file))
@@ -31,35 +33,37 @@ def main():
 
             account = util.get(split_element, 'split:account')
             value = util.get(split_element, 'split:value')
-            memo = util.get(split_element, 'split:memo')
             id = util.get(split_element, 'split:id')
 
             self.transaction = parent_transaction
             self.account = account
             amount_match = value.split('/')
-            self.amount = float(amount_match[0]) / float(amount_match[1])
+
+            numerator = float(amount_match[0])
+            denominator = float(amount_match[1])
+            self.decimal_places = amount_match[1].count('0')
+            self.amount = round(numerator / denominator, self.decimal_places)
 
             assertion_desc_match = regex.search(
-                '(' + args.assertion_regex + ')',
+                '(' + args.assertion_amount_regex + ')',
                 self.transaction.desc)
-
-            assertion_memo_match = regex.search(
-                '(' + args.assertion_regex + ')',
-                memo if memo else '')
-
-            assert not bool(assertion_desc_match) or not bool(assertion_memo_match)
 
             if assertion_desc_match:
                 assertion_amount_string = assertion_desc_match.group(0)
-            elif assertion_memo_match:
-                assertion_amount_string = assertion_memo_match.group(0)
-            else:
-                assertion_amount_string = None
-
-            if assertion_amount_string:
                 self.assertion_amount = float(assertion_amount_string)
             else:
                 self.assertion_amount = None
+                return # don't bother with the rest
+
+            self.assertion_start = pandas.to_datetime('1900-01-01')
+            if args.assertion_start_regex is not None:
+                assertion_start_match = regex.search(
+                    '(' + args.assertion_start_regex + ')',
+                    self.transaction.desc)
+
+                if assertion_start_match:
+                    assertion_start_string = assertion_start_match.group(0)
+                    self.assertion_start = pandas.to_datetime(assertion_amount_string)
 
         def is_assertion(self):
             return self.assertion_amount is not None
@@ -85,37 +89,33 @@ def main():
         trn = Transaction(transaction_element)
         all_splits += trn.splits
 
-    comp = lambda s : s.transaction.date
-    all_splits.sort(key=comp)
+    splits_df = pandas.DataFrame([(s.transaction.date, s.amount, s.account, s.decimal_places) for s in all_splits])
+    splits_df.columns = ['Date', 'Amount', 'Account', 'DecimalPlaces']
 
     for act_name, act_id in act_name_to_id_map:
-        splits = [s for s in all_splits if s.account == act_id]
-
         # now find balance assertions in the list of transactions
-        assertions = [s for s in splits if s.is_assertion()]
+        assertions = [s for s in all_splits if s.account == act_id and s.is_assertion()]
 
         print("found {} assertions in account '{}' ({})".format(len(assertions), act_name, act_id))
         assertions_count += len(assertions)
 
-        balance = 0
-        i = 0
-
         for assertion in assertions:
+            splits_subset = splits_df[
+                (splits_df.Date <= assertion.transaction.date) &
+                (splits_df.Date >= assertion.assertion_start) &
+                (splits_df.Account == act_id)]
 
-            for s in splits[i:]:
-
-                if s.transaction.date <= assertion.transaction.date:
-                    balance = round(balance + s.amount, args.d)
-                    i += 1
-                else:
-                    break
+            balance = round(
+                splits_subset.Amount.sum(),
+                splits_subset.DecimalPlaces.max())
 
             error = True if abs(balance - assertion.assertion_amount) > 0 else False
             error_count += int(error)
-            description = "    {}: checking value {} against balance of {}...{}".format(
+            description = "    {}: checking value {} against balance of {} (since {})...{}".format(
                 assertion.transaction.desc,
                 assertion.assertion_amount,
                 balance,
+                assertion.assertion_start,
                 "ERROR" if error else "OK")
 
             print(description)
